6523c6327639f86a073684164d97ea6ff5aab79a
ekaras <key.cpp@gmail.com>
2022-11-16 20:39:21 +0300

    slader work

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -11,10 +11,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var cenliusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
     
     
     @IBAction func sliderChanger(_ sender: UISlider) {
+        let temperatureCelcius = Int(round(sender.value))
+        cenliusLabel.text = "\(temperatureCelcius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9/5) + 32))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)ºF"
     }
     
 }
